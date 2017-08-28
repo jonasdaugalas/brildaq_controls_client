55768a1460d0c66b00b63ccb03e12778722c9533
jonasdaugalas <jonasdaugalas@gmail.com>
2017-08-28 14:11:20 +0200

    Raise build number

--- src/app-config-constants.ts
+++ src/app-config-constants.ts
@@ -1,5 +1,5 @@
 export const WEBLOGREADER_BASE = 'http://cmsrc-lumi.cms:5009/logs';
 export const AUTOUPDATE_INTERVAL = 57000;
 export const UPDATE_UNSTABLE_STATES_INTERVAL = 5000;
-export const BUILD_NUMBER = 2;
+export const BUILD_NUMBER = 61;
 export const BUILD_NUMBER_CHECK_INTERVAL = 3600000;
